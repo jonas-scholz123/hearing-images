6be32cb48f0b4825ef7f5e6e6e263cded711ccac
Jonas Scholz <jonas.scholz123@gmail.com>
2020-03-14 19:04:31 +0000

    improved design and button/enabling/disabling

--- gui.py
+++ gui.py
@@ -1,7 +1,6 @@
 from converter import Converter
 
 import time
-from tkinter import *
 from tkinter.ttk import Progressbar
 from tkinter import filedialog
 from playsound import playsound
@@ -9,6 +8,7 @@ import numpy as np
 import threading
 import pygame
 import pygame.camera
+from tkinter import *
 pygame.init()
 pygame.camera.init()
 import sounddevice as sd
@@ -16,18 +16,28 @@ import sounddevice as sd
 import queue
 
 class App(Tk):
+
+    ''' Graphical User Interface for taking/uploading images to convert'''
+
     def __init__(self):
-        super().__init__()
+        super().__init__() #Use Tk
 
         self.title("Make the visible audible")
 
+        # Initialise webcam
         pygame.camera.init()
         camlist = pygame.camera.list_cameras()
         self.cam = pygame.camera.Camera(camlist[0], (16, 16))
 
+        # Tone duration
+        self.tone_length = 5 #sec
+
+        # All buttons
         self.selected = IntVar()
-        self.rad1 = Radiobutton(self, text='Hilbert', value=1, variable=self.selected, indicatoron = True)
-        self.rad2 = Radiobutton(self, text='Snake', value=2, variable=self.selected, indicatoron = True)
+        self.rad1 = Radiobutton(self, text='Hilbert', value=1, variable=self.selected, indicatoron = True,
+                                    command = self.pressed_radios)
+        self.rad2 = Radiobutton(self, text='Snake', value=2, variable=self.selected, indicatoron = True,
+                                    command = self.pressed_radios)
 
         # Not functional yet
         #self.rad3 = Radiobutton(self ,text='Spectrogram', value=3, variable=selected, indicatoron = True)
@@ -37,34 +47,67 @@ class App(Tk):
                                  3: "spectrogram"}
 
 
-        self.rad1.grid(column=0, row=0)
-        self.rad2.grid(column=1, row=0)
+        self.rad1.grid(column=0, row = 1)
+        self.rad2.grid(column=1, row = 1)
+
+        #self.radio_title = Label(self, text="CONVERSION METHOD:", font=("system", 10), compound = CENTER)
+        #self.radio_title.grid(column = 0, row = 0, columnspan = 2)
+
+
         # Not functional yet
-        #self.rad3.grid(column=2, row=0)
+        #self.rad3.grid(column=2, row = 1)
 
-        self.btn1 = Button(self, text="Convert Image", command=self.convert_image, state = "disabled")
-        self.btn2 = Button(self, text="Upload Image", command=self.upload_image)
+        normal_button_width = 12
+        normal_button_height = 2
+
+        #self.btn1 = Button(self, text="Convert Image", command=self.convert_image, state = "disabled",
+                            #height = normal_button_height, width = 2* normal_button_width)
+
+        self.btn2 = Button(self, text="Upload Image", command=self.upload_image,
+                            height = normal_button_height, width = normal_button_width, state = "disabled")
+
         self.btn3 = Button(self, text="Play Sound", command=self.play_sound, state = "disabled")
-        self.btn_takeim = Button(self, text="Take Picture", command=self.take_image)
-        self.btn_begin_cap = Button(self, text="Begin Capture", command=self.begin_capture)
-        self.btn_end_cap = Button(self, text="Stop Capture", command=self.end_capture, state = "disabled")
-        self.btn1.grid(column=3, row=1)
-        self.btn2.grid(column=3, row=0)
-        self.btn3.grid(column= 3, row = 2)
-        self.btn_begin_cap.grid(column = 3, row = 3)
-        self.btn_end_cap.grid(column = 3, row = 4)
-        self.btn_takeim.grid(column = 4, row = 0)
 
+        self.btn_takeim = Button(self, text="Take Picture", command=self.take_image,
+                            height = normal_button_height, width = normal_button_width, state = "disabled")
+        self.btn_begin_cap = Button(self, text="Begin Capture", command=self.begin_capture, state = "disabled",
+                            height = normal_button_height, width = normal_button_width)
+        self.btn_end_cap = Button(self, text="Stop Capture", command=self.end_capture, state = "disabled",
+                            height = normal_button_height, width = normal_button_width)
+
+        #self.btn1.grid(column=3, row=1, columnspan = 2, sticky="nesw")
+        self.btn2.grid(column=3, row = 1)
+        self.btn3.grid(column= 3, row = 2, sticky="nesw", columnspan = 2)
+
+        self.btn_begin_cap.grid(column = 5, row = 1, sticky = "nesw")
+        self.btn_end_cap.grid(column = 5, row = 2)
+        self.btn_takeim.grid(column = 4, row = 1)
+
+        # Progress bar for converting images
         self.progress=Progressbar(self, orient=HORIZONTAL, length=150, mode='determinate')
-        self.progress.grid(column = 0, row = 1, padx = 10, columnspan = 2)
+        self.progress.grid(column = 0, row = 2, padx = 10, columnspan = 2)
+
 
         self.image_converted = False
 
+        #colours
+
+        self.green = "#b6ffad"
+        self.red   = "#FF9173"
+
+        # Seperators
+        #self.sep1 = ttk.Separator(self, orient=VERTICAL)
+        #self.sep2 = ttk.Separator(self, orient=VERTICAL)
+#
+        #self.sep1.grid(column=2, row=0, rowspan=3, sticky='ns', padx=3, pady = 3)
+        #self.sep2.grid(column=4, row=0, rowspan=1, sticky='ns', padx=3, pady = 3)
 
 
         return
 
     def take_image(self):
+        #self.btn1["state"] = "normal"
+        #self.btn1.configure(bg = self.green)
         self.cam.start()
         im = self.cam.get_image()
         im = pygame.transform.scale(im, (16, 16)) #make 16x16
@@ -74,22 +117,32 @@ class App(Tk):
         pygame.image.save(im, "./images/" + name + ".png")
         self.image_path = "./images/" + name + ".png"
         self.cam.stop()
+        self.tone_length = 5
         self.convert_image()
         return
 
+    def pressed_radios(self):
+        self.btn2["state"] = "normal"
+        self.btn_takeim["state"] = "normal"
+        self.btn_begin_cap["state"] = "normal"
+
+        self.btn2.config(bg = self.green)
+        self.btn_begin_cap.config(bg = self.green)
+        self.btn_takeim.config(bg = self.green)
 
     def convert_image(self, im = None):
         self.image_converted = False
         def real_convert_image():
             self.method = self.value_to_method[self.selected.get()]
-            self.converter = Converter(self.method, self.image_path)
+            self.converter = Converter(self.method, self.image_path, tone_length = self.tone_length)
             self.converter.set_null_colour = "white" #white background
             #self.converter.force_null = True
             self.audio = self.converter.convert()
             self.audio_path = self.converter.save_audio()
-            self.btn1['state'] = 'normal'
+            #self.btn1['state'] = 'normal'
             self.image_converted = True
             self.btn3["state"] = "normal"
+            self.btn3.configure(bg = self.green)
 
         def progress_bar_updater():
             while not self.image_converted:
@@ -97,7 +150,7 @@ class App(Tk):
                 self.progress["value"] = self.converter.progress
             self.progress["value"] = 100
 
-        self.btn1["state"] = "disabled"
+        #self.btn1["state"] = "disabled"
         self.btn3["state"] = "disabled"
         threading.Thread(target=real_convert_image).start()
         threading.Thread(target=progress_bar_updater).start()
@@ -106,12 +159,19 @@ class App(Tk):
 
     def play_sound(self):
         #playsound(self.audio_path)
-        sd.play(self.audio, 44100)
+        sd.play(list(self.audio), 44100)
         return
 
     def begin_capture(self):
+        self.tone_length = 1 #shorter tones for stream
         self.btn_end_cap["state"] = "normal"
         self.btn_begin_cap["state"] = "disabled"
+        self.btn2["state"] = "disabled"
+        self.btn2.configure(bg = "lightgrey")
+        self.btn_takeim.configure(bg = "lightgrey")
+        self.btn_takeim["state"] = "disabled"
+        self.btn_begin_cap.configure(bg = "lightgrey")
+        self.btn_end_cap.configure(bg = self.red)
         method = self.value_to_method[self.selected.get()]
         self.capture = Capture()
         self.audio_stream = AudioStream(method=method)
@@ -123,6 +183,12 @@ class App(Tk):
         self.audio_stream.stream.stop() #not while loop, harder to stop like capture
         self.btn_end_cap["state"] = "disabled"
         self.btn_begin_cap["state"] = "normal"
+        self.btn2["state"] = "normal"
+        self.btn2.configure(bg = self.green)
+        self.btn_takeim.configure(bg = self.green)
+        self.btn_takeim["state"] = "normal"
+        self.btn_end_cap.configure(bg = "lightgrey")
+        self.btn_begin_cap.configure(bg = self.green)
 
 #        def retrieve_im():
 #            self.is_recording = True
@@ -143,7 +209,8 @@ class App(Tk):
 
     def upload_image(self):
         self.image_path = filedialog.askopenfilename()
-        self.btn1["state"] = "normal"
+        #self.btn1["state"] = "normal"
+        #self.btn1.configure(bg = self.green)
         self.convert_image()
         return
 
